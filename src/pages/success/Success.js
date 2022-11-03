a2cd531c70a6eff1fede0ce48e1ebb5a1224a294
taofeek-ob <taofeek01@yahoo.com>
2022-11-03 15:29:53 +0100

    style: styled the success page

--- src/pages/success/Success.js
+++ src/pages/success/Success.js
@@ -1,15 +1,26 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FooterSection } from "../../components";
 import { runFireworks } from "../../lib/utils";
+import "./success.scss";
 
 const Success = () => {
   useEffect(() => {
     runFireworks();
   });
   return (
-    <div className="content">
+    <div className="success__content">
       <main>
-        <h1>Thank You for Visiting</h1>
+        <h1>Thank You for Messaging</h1>
+
+        <p>We will be in contact with you shortly</p>
+
+        <Link to="/">
+          {" "}
+          <button>Back to main Page</button>
+        </Link>
       </main>
+      <FooterSection />
     </div>
   );
 };
